eb78638c7fb862af42676720bab3dc643479a95b
Larissa <larissa@gasleuchte.de>
2019-02-25 14:31:31 +0100

    changed urls to fit to server

--- dataentry/settings.py
+++ dataentry/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'w@+3*qytb^c7p)_j=o(%uob2^0up93aa)w7p^i=(7ll3@d*w#x'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['militias-database.com', '.pythonanywhere.com', '127.0.0.1', 'militiasdb.sowi.uni-mannheim.de/militias-db/']
+ALLOWED_HOSTS = ['militias-database.com', '.pythonanywhere.com', '127.0.0.1', 'militiasdb.sowi.uni-mannheim.de']
 
 
 # Application definition
--- dataentry/urls.py
+++ dataentry/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('mildb.urls')),
+    path('militias-db/admin/', admin.site.urls),
+    path('militias-db/', include('mildb.urls')),
 ]
